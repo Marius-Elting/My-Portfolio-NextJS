a9af143d0a3aff97bdcea00de1ba805193bd50a2
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-15 14:46:18 +0200

    updated Header Component

---
+++ src/IData.ts
@@ -0,0 +1,66 @@
+interface IProject {
+    title: string
+    headline: string
+    description: string
+    image: string
+    link: string
+    stack: string
+}
+
+export interface IHeaderData {
+    home: string,
+    projects: string,
+    aboutMe: string,
+    contact: string
+}
+
+
+export interface IdataSingle {
+
+    Header: IHeaderData
+    Home: {
+        Headline: string
+        Headlinesub1: string
+        Headlinesub2: string
+    },
+    Projects: {
+        headline: string
+        showmore: string
+        ProjectData: Array<IProject>
+    },
+    AboutMe: {
+        AllHeadline: string
+        MyPastHeadline: string
+        MyPast: string
+        TheBootcampHeadline: string
+        TheBootcamp: string
+    },
+    ContactMe: {
+        Headline: string
+        YName: string
+        YEmail: string
+        YSubject: string
+        YMessage: string
+        Acceptpolicy: string
+        Infopolicy: string
+        policyLink: string
+        button: string
+        nameError: string
+        emailError: string
+    },
+    Privacy: {
+        Headline: string
+        Text: string
+    },
+    Other: {
+        contactButton: string
+        downloadCV: string
+    }
+}
+
+export interface IdataAll {
+    English: IdataSingle
+    German: IdataSingle
+}
+
+
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,106 @@
+import styles from "./Header.module.css";
+import Link from "next/link";
+import Data from "../../Languages.json";
+import { IHeaderData, IdataAll, IdataSingle } from "@/IData";
+import {
+  Dispatch,
+  FC,
+  ReactElement,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+interface IHeaderProps {
+  setLang: Dispatch<SetStateAction<string>>;
+  lang: string;
+}
+
+type Language = "English" | "German";
+const Header: FC<IHeaderProps> = ({ setLang, lang }): ReactElement => {
+  const [Header, setHeader] = useState<IHeaderData>();
+  const data: IdataAll = Data;
+  const burgerRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const lang: Language =
+      (sessionStorage.getItem("Language") as Language) ||
+      ("English" as Language);
+    setHeader(data[lang].Header);
+  }, []);
+
+  if (!Header) {
+    return <></>;
+  }
+  const navigated = () => {
+    if (!burgerRef.current?.checked) return;
+
+    burgerRef.current.checked = false;
+  };
+
+  return (
+    <header id={styles.HeaderWrapper}>
+      <input
+        className={styles.HeaderAcceptInfo}
+        type="checkbox"></input>
+      <Link href="/">
+        <h1>Marius Elting</h1>
+      </Link>
+      <input
+        ref={burgerRef}
+        className={styles.HeaderBurger}
+        type="checkbox"></input>
+      <nav id={styles.NavWrapper}>
+        <ul>
+          <li>
+            <Link
+              onClick={navigated}
+              className="underlineHover"
+              href="/">
+              {Header.home}
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={navigated}
+              className="underlineHover"
+              href="/allprojects">
+              {Header.projects}
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={navigated}
+              className="underlineHover"
+              href="/aboutme">
+              {Header.aboutMe}
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={navigated}
+              className="underlineHover"
+              href="/contactme">
+              {Header.contact}
+            </Link>
+          </li>
+          <li>
+            <span
+              className={lang === "German" ? styles.active : ""}
+              onClick={() => setLang("German")}>
+              DE
+            </span>{" "}
+            |{" "}
+            <span
+              className={lang === "English" ? styles.active : ""}
+              onClick={() => setLang("English")}>
+              EN
+            </span>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
