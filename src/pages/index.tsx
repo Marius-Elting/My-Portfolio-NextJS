261238ac248c79d6758addaa94486b0f1de8348a
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-15 12:51:03 +0200

    initialized Project with NextJS & Typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,34 @@
+import React, { FC, ReactElement } from "react";
+import ProjectOverwiew from "../components/Home/MyProjects/ProjectOverview";
+import TechStack from "../components/Home/TechStack/TechStack";
+import ThisIsMe from "../components/Home/ThisIsMe/ThisIsMe";
+import Head from "next/head";
+
+const Home: FC = (): ReactElement => {
+  return (
+    <main id="HomeWrapper">
+      <Head>
+        <meta charSet="utf-8" />
+        <title>Marius Elting - Webdeveloper - Home</title>
+        <link
+          rel="canonical"
+          href="https://mariuselting.dev"
+        />
+        <meta
+          name="description"
+          content="I'm Marius Elting, this is my Portfolio. 
+  Watch my TechStack, Projects and Contact me.
+  I'm an aspiring Full Stack Web Developer using the MERN Stack. This is my Homepage"
+        />
+      </Head>
+      {/* @ts-ignore TODO */}
+      <ThisIsMe />
+
+      <TechStack />
+      {/* @ts-ignore TODO */}
+      <ProjectOverwiew />
+    </main>
+  );
+};
+
+export default Home;
