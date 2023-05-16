883a3fcbfbb5bdfc993c030f03a7589572492bdc
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-16 15:29:58 +0200

    fixed import of old files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React, { FC, ReactElement } from "react";
 import ProjectOverwiew from "../components/Home/MyProjects/ProjectOverview";
 import TechStack from "../components/Home/TechStack/TechStack";
-import ThisIsMe from "../components/Home/ThisIsMe/ThisIsMe";
 import Head from "next/head";
 import Welcome from "@/components/Welcome/Welcome";
 
