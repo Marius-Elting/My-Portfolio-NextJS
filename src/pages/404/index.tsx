39789d772afb8496ca22f62bf68a7d6d62fc9b1a
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-16 13:51:25 +0200

    finished 404 Page

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Head from "next/head";
+import styles from "./404Page.module.css";
+import { FC, ReactElement } from "react";
+
+const Error404Page: FC = (): ReactElement => {
+  return (
+    <main id={styles.ErrorPageMainWrapper}>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>404 Page not found - Marius Elting - Webdeveloper</title>
+        <link
+          rel="canonical"
+          href="https://mariuselting.dev/404Error"
+        />
+        <meta
+          name="description"
+          content="This Page does not exists - 404 Error"
+        />
+      </Head>
+      <h1>404</h1>
+      <p>This Page does not exists</p>
+      <Link
+        className="underlineHover"
+        href="/">
+        Go Back to Home
+      </Link>
+    </main>
+  );
+};
+
+export default Error404Page;
